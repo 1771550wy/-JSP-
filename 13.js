52cd00ef7e1c73766963647775ad91c309c24f3f
1771550wy <38021966+1771550wy@users.noreply.github.com>
2018-08-29 16:31:05 +0800

    Create 13.js

---
+++ 13.js
@@ -0,0 +1,9 @@
+function findAllOccurrences(arr, target) {
+    var arr1=[];
+    for(i=0; i<arr.length; i++){
+        if(target==arr[i]){
+           arr1.push(i); 
+        }
+    }
+    return arr1;   
+}
