334b9ae2729987e7e8b680694204ea1afe2a41de
1771550wy <38021966+1771550wy@users.noreply.github.com>
2018-08-30 09:07:34 +0800

    Create 15.js

---
+++ 15.js
@@ -0,0 +1,9 @@
+function globals() {
+  myObject = {
+   //修改：var myObject = {
+   //在Javascript语言中，声明变量使用的都是关键字var，如果不使用var而直接声明变量，则该变量为全局变量。 
+      name : 'Jory'
+    };
+
+    return myObject;
+}
